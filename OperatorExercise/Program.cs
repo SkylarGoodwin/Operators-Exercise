56f95f2e9d2b10a141cc63fc44a44f109f17c808
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-02-05 03:59:15 -0600

    does anyone even read these?

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace OperatorExercise
+﻿// Exercise 1
+
+internal class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        int a = 17;
+        int b = 4;
+
+
+        Console.WriteLine("17 + 4 =", a+b,", 17 - 4 =", a-b, ", 17 X 4 =", a*b, 
+        ", 17 ÷ 4 =", a/b, "remainder ",a%b);
+
+// Exercise 2
+        Console.WriteLine("enter radius here");
+        var radius = double.Parse(Console.ReadLine());
+
+        double areaCircle = Math.PI * radius * radius;
+
+        Console.WriteLine("The area of a circle with a radius of ",radius," is ",areaCircle);
+
     }
 }
